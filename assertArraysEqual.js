21184648f9c5ee8c646102a589e3488097071b06
rod-nickle <rod@bitwise-it.com>
2024-10-16 09:46:26 -0600

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arr1, arr2) {
+  // Check array lengths first
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check that each element in each array are equal.
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]); // => fail
+assertArraysEqual(["1", "2", "3", "4"], ["1", "2", "3"]); // => fail
