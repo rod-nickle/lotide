334311590aace220bdae74bb845780f5ff7694cf
rod-nickle <rod@bitwise-it.com>
2024-10-18 10:05:39 -0600

    Added map.js

---
+++ map.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arr1, arr2) {
+  // Check array lengths first
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check that each element in each array are equal.
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Our version of the built-in Map function
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+// First character of each word
+const results1 = map(words, (word) => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']); // => pass
+
+// Uppercase each word
+const results2 = map(words, (word) => word.toUpperCase());
+assertArraysEqual(results2, ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']); // => pass
+
+// Prepend each word with "-"
+const results3 = map(words, (word) => "-" + word);
+assertArraysEqual(results3, ['-ground', '-control', '-to', '-major', '-tom']); // => pass
